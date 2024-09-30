ff8d354cf6079c48e2cc9d741783ac8d14acbbb9
camtcm <camila.carpio.mollo@ucsp.edu.pe>
2024-09-29 21:25:57 -0500

    configuración base de datos

---
+++ cvd_database.sql
@@ -0,0 +1,39 @@
+-- cv_database.sql
+CREATE TABLE user_info (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_apellidos VARCHAR(255) NOT NULL,
+    fecha_nacimiento DATE NOT NULL,
+    ocupacion VARCHAR(255) NOT NULL,
+    telefono VARCHAR(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    nacionalidad VARCHAR(100) NOT NULL,
+    nivel_ingles ENUM('básico', 'intermedio', 'avanzado', 'fluido') NOT NULL,
+    lenguajes_programacion TEXT NOT NULL,
+    aptitudes TEXT NOT NULL,
+    habilidades TEXT NOT NULL,
+    perfil TEXT NOT NULL
+);
+
+CREATE TABLE experiencia (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    titulo VARCHAR(255) NOT NULL,
+    descripcion TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_info(id)
+);
+
+CREATE TABLE formacion (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    titulo VARCHAR(255) NOT NULL,
+    descripcion TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_info(id)
+);
+
+CREATE TABLE idiomas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT,
+    idioma VARCHAR(100) NOT NULL,
+    nivel VARCHAR(50) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES user_info(id)
+);
